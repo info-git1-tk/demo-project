ce7d69020f901c72e91ef3d1a71746b4fde78f92
Informatik User 1 <info-git1-tk@treptowkolleg.de>
2023-05-17 06:46:02 +0200

    Attribute ergänzt

--- src/main/java/edu/inflk/algorithm/ConditionalStatementAlgorithm.java
+++ src/main/java/edu/inflk/algorithm/ConditionalStatementAlgorithm.java
@@ -3,15 +3,20 @@ package edu.inflk.algorithm;
 public class ConditionalStatementAlgorithm {
 
     /*
-    // TODO: Entwerfen Sie eine Methode mit einer einseitigen Verzweigung (if).
+    // TODO: Entwerfen Sie eine Methode mit einer einseitigen Verzweigung (if):
      */
+    private boolean aBoolean = true;
+    private boolean bBoolean = false;
 
     /*
     // TODO: Entwerfen Sie eine Methode mit einer zweiseitigen Verzweigung (if else).
+        Benutzen Sie die Attribute der vorherigen Aufgabe.
      */
 
     /*
     // TODO: Entwerfen Sie eine Methode mit einer mehrseitigen Verzweigung (switch case).
+        Verwenden Sie auch eine 'for'- oder 'for-each'-Schleife.
      */
+    private Integer[] numbers = {1,2,3,7,23,99,46,23};
 
 }
--- src/main/java/edu/inflk/algorithm/CountingLoopAlgorithm.java
+++ src/main/java/edu/inflk/algorithm/CountingLoopAlgorithm.java
@@ -3,11 +3,13 @@ package edu.inflk.algorithm;
 public class CountingLoopAlgorithm {
 
     /*
-    // TODO: Entwerfen Sie eine Methode mit einer Zählschleife (for).
+    // TODO: Entwerfen Sie eine Methode mit einer Zählschleife (for):
      */
+    private String[] words = {"Das","Treptow-Kolleg","ist","ein", "Institut","zur","Erlangung","der","Hochschulreife"};
 
     /*
-    // TODO: Entwerfen Sie eine Methode mit einer Schleife (for each).
+    // TODO: Entwerfen Sie eine Methode mit einer Schleife (for each):
      */
+    private Integer[] numbers = {1,2,3,7,23,99,46,23};
 
 }
